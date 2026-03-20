cbc397facfe35e25ce29437bb7897f9aeee03ff4
niyamanka <mankaniya3@gmail.com>
2026-03-20 20:47:18 +0530

    Niya Added UC5 Booking Request

--- App/src/UseCase4RoomSearch.java
+++ App/src/UseCase4RoomSearch.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class UseCase4RoomSearch {
+
+    public static void main(String[] args) {
+
+        RoomInventory inventory = new RoomInventory();
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        RoomSearchService searchService = new RoomSearchService();
+
+        searchService.searchAvailableRooms(
+                inventory,
+                single,
+                doubleRoom,
+                suite
+        );
+    }
 }
